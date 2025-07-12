300713eb08aeb68d7fa6ed7867239d1849900b42
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-07-12 18:27:54 +0530

    fix: typo

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <StarsBackground className="-z-10" />
 
       <h1 className="relative z-10 text-5xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-        Collections of Shadcn Components
+        Collection of Shadcn Components
       </h1>
 
       <Link
