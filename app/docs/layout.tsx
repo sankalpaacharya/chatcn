8d433c5de30461ec28be291bb0eb202627cae4f5
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-07-16 14:11:10 +0530

    add padding

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -7,7 +7,7 @@ export default function DocLayout({ children }: { children: ReactNode }) {
     <div className="flex h-full">
       <Sidebar />
       <main className="flex-1 overflow-auto">
-        <div className="prose dark:prose-invert w-full max-w-7xl mx-auto px-6 py-6">
+        <div className="prose dark:prose-invert w-full max-w-7xl mx-auto px-6 py-6 pb-16">
           {children}
         </div>
       </main>
