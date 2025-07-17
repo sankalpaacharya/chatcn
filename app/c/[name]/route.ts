97c9e282f77a6702ecfdf7e257c994fb6ea819ac
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-07-18 00:43:28 +0530

    add posthog

---
+++ app/c/[name]/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest } from "next/server";
+import { readFile } from "node:fs/promises";
+import path from "node:path";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { name: string } }
+) {
+  const name = params.name;
+
+  const filePath = path.join(process.cwd(), "public", "r", `${name}.json`);
+  try {
+    const content = await readFile(filePath, "utf-8");
+    return new Response(content, {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (err) {
+    console.error(err);
+    return new Response(JSON.stringify({ error: "File not found" }), {
+      status: 404,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
