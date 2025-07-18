181b794dc3d308dae8e413988c03631e9496531e
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-07-18 14:15:17 +0530

    add manual installation

---
+++ components/codesource.tsx
@@ -0,0 +1,12 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { CodeBlock } from "./codeblock";
+
+export default async function CodeSource({ component }: { component: string }) {
+  const filePath = path.join(
+    process.cwd(),
+    `registry/new-york/${component}/${component}.tsx`
+  );
+  const fileContent = await fs.readFile(filePath, "utf-8");
+  return <CodeBlock lang="tsx">{fileContent}</CodeBlock>;
+}
