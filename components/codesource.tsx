593377ebfdbff57dc43b32dc0ec077d1e18fe474
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-08-23 10:38:41 +0530

    fix: simplify import paths for CodeBlock and update file path resolution in CodeSource

--- components/codesource.tsx
+++ components/codesource.tsx
@@ -5,7 +5,7 @@ import { CodeBlock } from "./codeblock";
 export default async function CodeSource({ component }: { component: string }) {
   const filePath = path.join(
     process.cwd(),
-    `registry/new-york/${component}/${component}.tsx`
+    `registry/new-york/${component}.tsx`
   );
   const fileContent = await fs.readFile(filePath, "utf-8");
   return <CodeBlock lang="tsx">{fileContent}</CodeBlock>;
