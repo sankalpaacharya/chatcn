e5014a3bc42b3b3760bd58245bfadca11428ba4e
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-10-08 22:13:35 +0530

    feat(command block): separate clipboard from the title

--- registry/new-york/command-block.tsx
+++ registry/new-york/command-block.tsx
@@ -51,13 +51,48 @@ export function CommandBlock({ className, children }: CommandBlockProps) {
 }
 
 type CommandBlockHeaderProps = {
-  children: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 export function CommandBlockHeader({ children }: CommandBlockHeaderProps) {
+  const { activeCommand, copied, setCopied } = useCommandBlockContext();
+
+  const copyToClipboard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  };
+
   return (
     <>
-      {children}
+      <div className="flex items-center justify-between px-3 sm:px-4 py-1.5 sm:py-2">
+        <div className="flex items-center space-x-2">{children}</div>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="h-7 w-7 sm:h-8 sm:w-8 p-0"
+              onClick={() => copyToClipboard(activeCommand)}
+            >
+              {copied ? (
+                <Check className="size-3.5 sm:size-4.5" />
+              ) : (
+                <Clipboard className="size-4 sm:size-5" />
+              )}
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p className="text-xs sm:text-sm">
+              {copied ? "Copied!" : "Copy to Clipboard"}
+            </p>
+          </TooltipContent>
+        </Tooltip>
+      </div>
       <Separator />
     </>
   );
@@ -72,51 +107,16 @@ export function CommandBlockTitle({
   children,
   showTerminalIcon = true,
 }: CommandBlockTitleProps) {
-  const { activeCommand, copied, setCopied } = useCommandBlockContext();
-
-  const copyToClipboard = async (text: string) => {
-    try {
-      await navigator.clipboard.writeText(text);
-      setCopied(true);
-      setTimeout(() => setCopied(false), 2000);
-    } catch (err) {
-      console.error("Failed to copy text: ", err);
-    }
-  };
-
   if (!children && !showTerminalIcon) return null;
 
   return (
-    <div className="flex items-center justify-between px-3 sm:px-4 py-1.5 sm:py-2">
-      <div className="flex items-center space-x-2">
-        {showTerminalIcon && (
-          <SquareTerminal className="text-muted-foreground size-4 sm:size-5" />
-        )}
-        {children && (
-          <span className="font-medium text-sm sm:text-base">{children}</span>
-        )}
-      </div>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button
-            variant="ghost"
-            size="sm"
-            className="h-7 w-7 sm:h-8 sm:w-8 p-0"
-            onClick={() => copyToClipboard(activeCommand)}
-          >
-            {copied ? (
-              <Check className="size-3.5 sm:size-4.5" />
-            ) : (
-              <Clipboard className="size-4 sm:size-5" />
-            )}
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p className="text-xs sm:text-sm">
-            {copied ? "Copied!" : "Copy to Clipboard"}
-          </p>
-        </TooltipContent>
-      </Tooltip>
+    <div className="flex items-center space-x-2">
+      {showTerminalIcon && (
+        <SquareTerminal className="text-muted-foreground size-4 sm:size-5" />
+      )}
+      {children && (
+        <span className="font-medium text-sm sm:text-base">{children}</span>
+      )}
     </div>
   );
 }
@@ -137,7 +137,12 @@ export function CommandBlockContent({
   }, [command, setActiveCommand]);
 
   return (
-    <div className="px-3 sm:px-4 py-3 sm:py-4 font-mono bg-card overflow-x-auto text-primary">
+    <div
+      className={cn(
+        "px-3 sm:px-4 py-3 sm:py-4 font-mono bg-card overflow-x-auto text-primary",
+        className
+      )}
+    >
       <p className="break-words whitespace-pre-wrap text-xs sm:text-sm md:text-base">
         {command}
       </p>
