6cf856df4db7a6192dc5d7e6a7641299e432e5a0
Nishit Barbhaya <nishitbarbhaya06@gmail.com>
2025-10-09 16:45:50 +0530

    refactor: signup using compound design pattern

--- preview/signup-preview.tsx
+++ preview/signup-preview.tsx
@@ -1,10 +1,74 @@
-import SignUpForm from '@/registry/new-york/signup'
-import React from 'react'
-
+import SignUpForm from "@/registry/new-york/signup";
+import React from "react";
+import { ArrowUpRight } from "lucide-react";
 const SignupPreview = () => {
   return (
-    <SignUpForm/>
-  )
-}
+    <SignUpForm>
+      <SignUpForm.Header
+        heading="Create an account"
+        subheading="Enter your information below to create your account"
+      />
 
-export default SignupPreview
+      <SignUpForm.Field>
+        <SignUpForm.Label htmlFor="fullName">Full Name</SignUpForm.Label>
+        <SignUpForm.Input
+          name="fullName"
+          type="text"
+          placeholder="Jack Sparrow"
+          minLength={2}
+        />
+      </SignUpForm.Field>
+
+      <SignUpForm.Field>
+        <SignUpForm.Label htmlFor="email">Email</SignUpForm.Label>
+        <SignUpForm.Input
+          name="email"
+          type="email"
+          placeholder="jack@example.com"
+        />
+        <SignUpForm.HelperText>
+          We'll use this to contact you. We won’t share your email with anyone
+          else.
+        </SignUpForm.HelperText>
+      </SignUpForm.Field>
+
+      <SignUpForm.Field>
+        <SignUpForm.Label htmlFor="password">Password</SignUpForm.Label>
+        <SignUpForm.Input
+          name="password"
+          type="password"
+          placeholder="••••••"
+          minLength={8}
+        />
+        <SignUpForm.HelperText>
+          Must be at least 8 characters long.
+        </SignUpForm.HelperText>
+      </SignUpForm.Field>
+
+      <SignUpForm.Field>
+        <SignUpForm.Label htmlFor="confirmPassword">
+          Confirm Password
+        </SignUpForm.Label>
+        <SignUpForm.Input
+          name="confirmPassword"
+          type="password"
+          placeholder="••••••"
+          minLength={8}
+        />
+        <SignUpForm.HelperText>
+          Please confirm your password.
+        </SignUpForm.HelperText>
+      </SignUpForm.Field>
+
+      <SignUpForm.Actions>
+        <SignUpForm.SubmitButton text="Create Account" redirectTo="#" />
+        <SignUpForm.SocialButton provider="Google" icon={<ArrowUpRight />} redirectTo="/api/auth/google"/>
+        <SignUpForm.SocialButton provider="Github" icon={<ArrowUpRight />} redirectTo="/api/auth/google"/>
+      </SignUpForm.Actions>
+
+      <SignUpForm.Footer footer="Already have an account?" footerLink={{text:"Sign in here.", redirectTo:"#"}} />
+    </SignUpForm>
+  );
+};
+
+export default SignupPreview;
