eeb570dd717427da3be772c97b368d3f8b88c33b
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-10-26 01:10:54 +0530

    fix: md format

--- preview/model-preview.tsx
+++ preview/model-preview.tsx
@@ -1,18 +1,36 @@
 import React from "react";
-import { Model, ModelScene, ModelCamera, ModelLighting, ModelControls, ModelContent } from "@/components/chatcn/model";
+import {
+  Model,
+  ModelScene,
+  ModelCamera,
+  ModelLighting,
+  ModelControls,
+  ModelContent,
+} from "@/components/chatcn/model";
 
 const ModelPreview = () => {
   return (
-    <ModelContent height="100vh" width="100%">
-      <Model src="/abstract.glb" position={[0, 0, 0]} rotation={[0,-Math.PI/4,0]} scale={1} float={false} />
+    <ModelContent height="30px" width="100%">
+      <Model
+        src="/abstract.glb"
+        position={[0, 0, 0]}
+        rotation={[0, -Math.PI / 4, 0]}
+        scale={1}
+        float={false}
+      />
       <ModelScene bgColor="#0a0a0a" env="city" />
       <ModelCamera fov={50} position={[0, 0, 5]} />
       <ModelLighting type="studio" shadow />
-      <ModelControls autoRotate rotationSpeed={1} zoom={false} reverse={false} 
-      maxPolarAngle={Math.PI / 2}
-      minPolarAngle={Math.PI / 4}
-      minAzimuthAngle={-Math.PI /4}
-      maxAzimuthAngle={Math.PI / 8}/>
+      <ModelControls
+        autoRotate
+        rotationSpeed={1}
+        zoom={false}
+        reverse={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 4}
+        minAzimuthAngle={-Math.PI / 4}
+        maxAzimuthAngle={Math.PI / 8}
+      />
     </ModelContent>
   );
 };
