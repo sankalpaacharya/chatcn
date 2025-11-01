4d5f691bc2456cf9a9b2d86bf7eeeb09f58c6e1d
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-11-01 11:04:07 +0530

    fix: clean up test route

--- app/(main)/test/page.tsx
+++ app/(main)/test/page.tsx
@@ -1,10 +1,5 @@
 "use client";
-import { FileManager } from "@/components/chatcn/system/file-manager";
 
 export default function Page() {
-  return (
-    <div>
-      <FileManager />
-    </div>
-  );
+  return <div></div>;
 }
