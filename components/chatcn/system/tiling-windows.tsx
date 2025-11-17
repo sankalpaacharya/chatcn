b143b4b504c279ef0c9988ae88e308055c388576
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-11-17 11:28:19 +0530

    chore: change og image

--- components/chatcn/system/tiling-windows.tsx
+++ components/chatcn/system/tiling-windows.tsx
@@ -1,8 +1,5 @@
-import React from 'react'
-
+import React from "react";
 
 export default function TilingWindow() {
-  return (
-    <div>tiling-windows</div>
-  )
+  return <div>tiling-windows</div>;
 }
